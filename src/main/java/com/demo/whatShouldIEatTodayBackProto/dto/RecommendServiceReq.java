659c355e823ec2f1986b9406609893037b1c6e1f
KJ <abc8339265@gmail.com>
2021-10-14 21:34:47 +0900

    Delete legacy code

--- src/main/java/com/demo/whatShouldIEatTodayBackProto/dto/RecommendServiceReq.java
+++ src/main/java/com/demo/whatShouldIEatTodayBackProto/dto/RecommendServiceReq.java
@@ -11,18 +11,4 @@ public class RecommendServiceReq <T> {
     private T longitude;
     private T range;
     private List<T> categories;
-
-//    public MultiValueMap<String, T> getMultiValueMap() {
-//        LinkedMultiValueMap<String, T> map = new LinkedMultiValueMap<>();
-//
-//        map.add("latitude", latitude);
-//        map.add("longitude", longitude);
-//        map.add("range", range);
-//        for (T category : categories) {
-//            map.add("categories", category);
-//        }
-//
-//        return map;
-//    }
-
 }
